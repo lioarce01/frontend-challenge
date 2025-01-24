3a656335f1a9ac8f972186acee4ceed6b6aaea0e
lioarce01 <lioarce1@gmail.com>
2025-01-24 14:45:41 -0300

    ready to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/frontend-challenge/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
